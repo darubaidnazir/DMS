6e2a62d027831d6f4c4b8574e96673da5e27ca59
user.email <faizfarooq5@gmail.com>
2022-04-25 11:45:41 +0530

    Added Validation to Signup Page

--- Cordinator/index.js
+++ Cordinator/index.js
@@ -6,6 +6,7 @@ let password1 = document.querySelector("#password1");
 let password2 = document.querySelector("#password2");
 
 
+// For Animation
 
 $("#login").click(function() {
     $( "#loginPage" ).slideToggle( "slow");
@@ -14,6 +15,10 @@ $("#login").click(function() {
   $("#signUp").click(function() {
     $( "#signupPage" ).slideToggle( "slow");
   });
+
+
+
+
 login.addEventListener("click",()=>
 {
     loginPage.style.display = "block";
@@ -27,6 +32,9 @@ login.addEventListener("click",()=>
 
 })
 
+
+
+
 signUp.addEventListener("click",()=>
 {
     loginPage.style.display = "none";
@@ -38,3 +46,68 @@ signUp.addEventListener("click",()=>
     login.style.opacity=".5";
     signUp.style.opacity="1";
 })
+
+
+
+// Validation Section
+
+// Name Validation 
+$(document).ready(function(){
+  var $regexname=/^([a-zA-Z'-'\s]{10,})$/;
+  $('.name').on('keypress keydown keyup',function(){
+           if (!$(this).val().match($regexname)) {
+            $('.nmsg').removeClass('nhidden');
+            $('.nmsg').show();
+            $('.name').css("border","2px solid red");
+               
+           }
+         else{
+             
+              $('.nmsg').addClass('nhidden');
+            $('.name').css("border","2px solid green");
+
+             }
+       });
+});
+
+// Password1 Validation 
+$(document).ready(function(){
+  var $passregexname=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  $('.pass').on('keypress keydown keyup',function(){
+           if (!$(this).val().match($passregexname)) {
+            $('.pmsg').removeClass('phidden');
+            $('.pmsg').show();
+            $('.pass').css("border","2px solid red");
+               
+           }
+         else{
+             
+              $('.pmsg').addClass('phidden');
+            $('.pass').css("border","2px solid green");
+
+             }
+       });
+});
+
+// Employee Validation 
+$(document).ready(function(){
+  var $employeregexname=/^([0-9]+.{8,})$/;
+  $('.emplo').on('keypress keydown keyup',function(){
+           if (!$(this).val().match($employeregexname)) {
+            $('.emsg').removeClass('ehidden');
+            $('.emsg').show();
+            $('.emplo').css("border","2px solid red");
+               
+           }
+         else{
+             
+              $('.emsg').addClass('ehidden');
+            $('.emplo').css("border","2px solid green");
+
+             }
+       });
+});
+
+
+
+
