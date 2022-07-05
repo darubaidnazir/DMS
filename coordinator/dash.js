3814a820464420747f07097b38947e58d6f41325
Dar Ubaid Nazir <darubaidnazir786@gmail.com>
2022-07-05 18:42:13 +0530

    Added All Modal boxes for the first version for Backend-code

--- coordinator/dash.js
+++ coordinator/dash.js
@@ -9,16 +9,17 @@ const switchLabelText = switchLabel.querySelector("span:last-child");
 const collapsedClass = "collapsed";
 const lightModeClass = "light-mode";
 
-/*TOGGLE HEADER STATE*/
+/*TOGGLE HEADER STATE
 collapseBtn.addEventListener("click", function () {
   body.classList.toggle(collapsedClass);
+  
   this.getAttribute("aria-expanded") == "true"
     ? this.setAttribute("aria-expanded", "false")
     : this.setAttribute("aria-expanded", "true");
   this.getAttribute("aria-label") == "collapse menu"
     ? this.setAttribute("aria-label", "expand menu")
     : this.setAttribute("aria-label", "collapse menu");
-});
+});*/
 
 /*TOGGLE MOBILE MENU*/
 toggleMobileMenu.addEventListener("click", function () {
